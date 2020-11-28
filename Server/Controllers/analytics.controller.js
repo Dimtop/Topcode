178e8721857672b40659d04186409a1aaeb127a1
Dimtop <topalidisdofcl@gmail.com>
2020-11-29 00:12:26 +0200

    Contact tracking

--- Server/Controllers/analytics.controller.js
+++ Server/Controllers/analytics.controller.js
@@ -5,6 +5,7 @@ var geoip = require('geoip-lite');
 exports.trackContactButtonPress = async function(req,res){
 
     var geo  =geoip.lookup(req.ip);
+    console.log(geo)
     if(geo){
         fs.appendFileSync(path.join(__dirname,"../Data","contactButtonTrack.txt"),"\n"+ geo.country +" " + geo.city + " " +geo.region + " " + req.ip + " " + req.headers["user-agent"] );
     
