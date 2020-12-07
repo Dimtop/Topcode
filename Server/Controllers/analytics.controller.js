5b9da8171b0afedfdf4f01dea20847ed09dcfdff
Dimtop <topalidisdofcl@gmail.com>
2020-12-07 20:47:54 +0200

    Color change

--- Server/Controllers/analytics.controller.js
+++ Server/Controllers/analytics.controller.js
@@ -4,13 +4,15 @@ var geoip = require('geoip-lite');
 
 exports.trackContactButtonPress = async function(req,res){
 
-    var geo  =geoip.lookup(req.ip);
+    var geo  =geoip.lookup(req.headers['x-forwarded-for']);
     console.log(geo)
     if(geo){
-        fs.appendFileSync(path.join(__dirname,"../Data","contactButtonTrack.txt"),"\n"+ geo.country +" " + geo.city + " " +geo.region + " " + req.ip + " " + req.headers["user-agent"] );
+        fs.appendFileSync(path.join(__dirname,"../Data","contactButtonTrack.txt"),"\n\n"+ geo.country +" " + geo.city + " " +geo.region + " " + req.ip + " " + req.headers["user-agent"]+" " + req.headers['x-forwarded-for'] );
     
     }
-
+	else{
+	 fs.appendFileSync(path.join(__dirname,"../Data","contactButtonTrack.txt"),"\n\nViewed but no data tracked. " + req.ip +" " + req.connection.remoteAddress+ " " + req.headers['x-forwarded-for']);
+	}
     res.send({success:true})
    
 }
