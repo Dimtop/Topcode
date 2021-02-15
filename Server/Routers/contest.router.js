b8f7cee245eb8bd8f87ac7f56fc44793b56e150c
Dimtop <44418460+Dimtop@users.noreply.github.com>
2021-02-15 19:13:04 +0200

    Delete Server directory

--- Server/Routers/contest.router.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-var express = require('express')
-var router = express.Router();
-
-const contestController = require("../Controllers/contest.controller");
-
-router.post("/",contestController.postContest)
-
-module.exports = router;
